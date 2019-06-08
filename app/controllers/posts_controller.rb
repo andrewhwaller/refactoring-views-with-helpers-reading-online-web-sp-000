f5a4ad66634c72aff513259238fcc36f0086c85f
Andrew Waller <andrewhwaller@gmail.com>
2019-06-08 03:16:44 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,6 +30,6 @@ class PostsController < ApplicationController
 	private
 
 	def post_params
-		params.require(:post).permit(:username, :email, :password, :salt, :encrypted_password)
+		params.require(:post).permit(:title, :description)
 	end
 end
